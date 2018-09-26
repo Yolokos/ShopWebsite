081133042cb2617d5dbf6d928ee2bbbd7b71205e
Yolokos <mixan19931@gmail.com>
2018-09-26 13:54:07 +0300

    shop

---
+++ BookStoreWebApi/Controllers/ShopController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookStoreWebApi.Models;
+
+namespace BookStoreWebApi.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public ShopController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Store() => View(db.Books.ToList());
+
+        public async Task<IActionResult> InfoBook(string ISBN)
+        {
+            if(!string.IsNullOrEmpty(ISBN))
+            {
+                var book = await db.Books.FirstOrDefaultAsync(p => p.ISBN == ISBN);
+                if(book != null)
+                {
+                    return View(book);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Такой книги не существует");
+                }
+            }
+            return NotFound();
+        }
+
+
+
+        public async void AddToCart(string ISBN)
+        {
+            var book = await db.Books.FirstOrDefaultAsync(p => p.ISBN == ISBN);
+
+        }
+    }
+}
